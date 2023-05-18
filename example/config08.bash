cd8d432c721a356929bb01958be5fd1bdd789f3f
Paul Stodghill <paul.stodghill@usda.gov>
2023-05-18 08:47:59 -0400

    An FDR cutoff of 1 is not publishable; it's just an example.

--- example/config08.bash
+++ example/config08.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-run_deseq2 -f 1.0 -c 2.0 -t publishable WT:WT1 WT:WT2 WT:WT3 5255:52551 5255:52552 5255:52553
+run_deseq2 -f 1.0 -c 2.0 -t example WT:WT1 WT:WT2 WT:WT3 5255:52551 5255:52552 5255:52553
 
 run_deseq2 -f -1 -c 1.5 -t unfiltered WT:WT1 WT:WT2 WT:WT3 5255:52551 5255:52552 5255:52553
