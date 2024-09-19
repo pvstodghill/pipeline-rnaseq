7b9419c47dedda45fd9277caed1ee1cde076ed54
Paul Stodghill <paul.stodghill@usda.gov>
2024-09-19 15:40:06 -0400

    config.template.mk: Added some documentation.

--- config.template.mk
+++ config.template.mk
@@ -1,50 +1,66 @@
-DATA := data
+########################################################################
+# Basic parameters
+########################################################################
 
-THREADS := $(shell nproc)
+# # Where to put output files
+# DATA := data
 
-GENOME_FNA := fixme.fna
-GENOME_GTF := fixme.gtf
+# # How many threads to use
+# THREADS := $(shell nproc)
 
-ADDITIONAL_GTF:=
+# # The genome
+# GENOME_FNA := fixme.fna
+# GENOME_GTF := fixme.gtf
+# # Any additional annotations
+# ADDITIONAL_GTF:=
 
-SAMPLE_NAMES := WT1 WT2 WT3 Mutant1 Mutant2 Mutant3
+########################################################################
+# Illumina parameters
+########################################################################
 
-R1_WT1 := /path/to/raw/reads/WT1_R1.fastq.gz
-R2_WT1 := /path/to/raw/reads/WT1_R2.fastq.gz
-R1_WT2 := /path/to/raw/reads/WT2_R1.fastq.gz
-R2_WT2 := /path/to/raw/reads/WT2_R2.fastq.gz
-R1_WT3 := /path/to/raw/reads/WT3_R1.fastq.gz
-R2_WT3 := /path/to/raw/reads/WT3_R2.fastq.gz
-R1_Mutant1 := /path/to/raw/reads/Mutant1_R1.fastq.gz
-R2_Mutant1 := /path/to/raw/reads/Mutant1_R2.fastq.gz
-R1_Mutant2 := /path/to/raw/reads/Mutant2_R1.fastq.gz
-R2_Mutant2 := /path/to/raw/reads/Mutant2_R2.fastq.gz
-R1_Mutant3 := /path/to/raw/reads/Mutant3_R1.fastq.gz
-R2_Mutant3 := /path/to/raw/reads/Mutant3_R2.fastq.gz
+# SAMPLE_NAMES := WT1 WT2 WT3 Mutant1 Mutant2 Mutant3
 
-# FASTP_ARGS := --trim_front1 1 --trim_front2 1 --adapter_sequence CTGTCTCTTATACACATCT
+# R1_WT1 := /path/to/raw/reads/WT1_R1.fastq.gz
+# R2_WT1 := /path/to/raw/reads/WT1_R2.fastq.gz
+# R1_WT2 := /path/to/raw/reads/WT2_R1.fastq.gz
+# R2_WT2 := /path/to/raw/reads/WT2_R2.fastq.gz
+# R1_WT3 := /path/to/raw/reads/WT3_R1.fastq.gz
+# R2_WT3 := /path/to/raw/reads/WT3_R2.fastq.gz
+# R1_Mutant1 := /path/to/raw/reads/Mutant1_R1.fastq.gz
+# R2_Mutant1 := /path/to/raw/reads/Mutant1_R2.fastq.gz
+# R1_Mutant2 := /path/to/raw/reads/Mutant2_R1.fastq.gz
+# R2_Mutant2 := /path/to/raw/reads/Mutant2_R2.fastq.gz
+# R1_Mutant3 := /path/to/raw/reads/Mutant3_R1.fastq.gz
+# R2_Mutant3 := /path/to/raw/reads/Mutant3_R2.fastq.gz
 
-# ORIENTATION := forward # reads map to "sense"
-# ORIENTATION := reverse # reads map to "antisense"
-ORIENTATION := # we'll figure it out
+# # FASTP_ARGS := --trim_front1 1 --trim_front2 1 --adapter_sequence CTGTCTCTTATACACATCT
 
-COUNT_FEATURE := gene
+# # How do the reads map to the genome?
+# # ORIENTATION := forward # reads map to "sense"
+# # ORIENTATION := reverse # reads map to "antisense"
+# ORIENTATION := # we'll figure it out heuristically
 
-EXP_NAMES := normal unfiltered
+########################################################################
+# Diff exp parameters
+########################################################################
 
-FDR_CUTOFF_normal := 0.05
-PVALUE_CUTOFF_normal := -1
-FOLDCHANGE_CUTOFF_normal := 2.0
-CONTROL_NAME_normal := WT
-CONTROL_SAMPLES_normal := WT1 WT2 WT3
-TREATMENT_NAME_normal := DeltaMutant
-TREATMENT_SAMPLES_normal := Mutant1 Mutant2 Mutant3
+# COUNT_FEATURE := gene
 
-FDR_CUTOFF_unfiltered := -1
-PVALUE_CUTOFF_unfiltered := -1
-FOLDCHANGE_CUTOFF_unfiltered := 1.5
-CONTROL_NAME_unfiltered := ${CONTROL_NAME_normal}
-CONTROL_SAMPLES_unfiltered := ${CONTROL_SAMPLES_normal}
-TREATMENT_NAME_unfiltered := ${TREATMENT_NAME_normal}
-TREATMENT_SAMPLES_unfiltered := ${TREATMENT_SAMPLES_normal}
+# EXP_NAMES := normal unfiltered
+
+# FDR_CUTOFF_normal := 0.05
+# PVALUE_CUTOFF_normal := -1
+# FOLDCHANGE_CUTOFF_normal := 2.0
+# CONTROL_NAME_normal := WT
+# CONTROL_SAMPLES_normal := WT1 WT2 WT3
+# TREATMENT_NAME_normal := Mutant
+# TREATMENT_SAMPLES_normal := Mutant1 Mutant2 Mutant3
+
+# FDR_CUTOFF_unfiltered := -1
+# PVALUE_CUTOFF_unfiltered := -1
+# FOLDCHANGE_CUTOFF_unfiltered := 1.5
+# CONTROL_NAME_unfiltered := ${CONTROL_NAME_normal}
+# CONTROL_SAMPLES_unfiltered := ${CONTROL_SAMPLES_normal}
+# TREATMENT_NAME_unfiltered := ${TREATMENT_NAME_normal}
+# TREATMENT_SAMPLES_unfiltered := ${TREATMENT_SAMPLES_normal}
 
